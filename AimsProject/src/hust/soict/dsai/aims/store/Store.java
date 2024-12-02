cb7a4062a12d45cbcb22c0203e9b81bda0928331
Quynh <101940232+kwspringkle@users.noreply.github.com>
2024-12-03 01:55:45 +0700

    update store and cart class

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -1,49 +1,32 @@
 package hust.soict.dsai.aims.store;
 
+import java.util.ArrayList;
+import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
 public class Store {
     private static final int MAX_ITEMS = 100;  
-    private DigitalVideoDisc[] itemsinStore;
-    private int itemCount = 0;
-    
-    public Store() {
-        itemsinStore = new DigitalVideoDisc[MAX_ITEMS];
-    }
+    private ArrayList<Media> itemsinStore = new ArrayList<Media>();
 
-    public void addDVD(DigitalVideoDisc dvd) {
-        if (itemCount >= MAX_ITEMS) {
+    public void addMedia(Media media) {
+        if (itemsinStore.size() >= MAX_ITEMS) {
             System.out.println("The store is full!");
             return;
         }
-        
-        itemsinStore[itemCount++] = dvd;
-        System.out.println("DVD added successfully to the store!");
+        if(itemsinStore.contains(media)) {
+    		System.out.println(media.getTitle() + " already exists!");
+    		return;
+    	}
+        itemsinStore.add(media);
+        System.out.println(media.getTitle() + " is added successfully to the store!");
     }
 
-    public void removeDVD(DigitalVideoDisc dvd) {
-        if (itemCount == 0) {  
-            System.out.println("The store is empty!");
-            return;
-        }
-        
-        boolean found = false;
-        for (int i = 0; i < itemCount; i++) {
-            if (itemsinStore[i] == dvd) {
-            
-                for (int j = i; j < itemCount - 1; j++) {
-                    itemsinStore[j] = itemsinStore[j + 1];
-                }
-                itemsinStore[itemCount - 1] = null;
-                itemCount--;
-                found = true;
-                System.out.println("DVD removed successfully from the store");
-                break;
-            }
-        }
-
-        if (!found) {
-            System.out.println("DVD not found in the store");
-        }
+    public void removeMedia(Media media) {
+    	if(!itemsinStore.contains(media)) {
+    		System.out.println(media.getTitle() + " is not in the store");
+    		return;
+    	}
+    	itemsinStore.remove(media);
+    	System.out.println(media.getTitle() + " is removed successfully");
     }
 }
--- AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
@@ -8,20 +8,19 @@ public class CartTest {
 		Cart cart = new Cart();
 		
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation", "Roger Allers", 87, 19.95f);
-		cart.addDigitalVideoDisc(dvd1);
+		cart.addMedia(dvd1);
 		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star wars","Science Fiction", "George Lucas", 87, 24.95f);
-		cart.addDigitalVideoDisc(dvd2);
+		cart.addMedia(dvd2);
 		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		cart.addDigitalVideoDisc(dvd3);
+		cart.addMedia(dvd3);
 		
 		//Test print method
 		cart.printCart();
 		//test search method
+		
 		cart.searchByID(0);
 		cart.searchByID(2);
 		cart.searchByTitle("Aladin");
 		cart.searchByTitle("Sofia the first");
-		
-		
 	}
 }
